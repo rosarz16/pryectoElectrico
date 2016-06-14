a2a5ec5ab0286701e4a9bcba8dd4fc95650570e1
rosarz16 <rosarioi16@gmail.com>
2016-06-14 03:39:53 -0600

    Add files via upload

--- Interfaz0.py
+++ Interfaz0.py
@@ -5,22 +5,39 @@ import ttk
 # import tkSimpleDialog
 # import tkMessageBox
 
+# Inicializar banderas
+start_calcs = 0
+
+
+def actualizar():
+    start_calcs = 1
+    App.update()
+    return start_calcs
+
 App = Tk()
 App.geometry("1700x600+-10+20")
 App.title("Interfaz gráfica")
+print "start_calcs es: ", start_calcs
+if start_calcs == 0:
+    Argumentos = ['Optimizar', 'Jdomax', 'Jrmax']
+# primer_Argumento='Optimizar'
+# segundo_Argumento='Jdomax'
+# tercer_Argumento='Jrmax'
 
 # Var declaration
 label_x_init = 120
 label_y_init = 150
 label_x_offset = 0
-box_x_init = 175
+box_x_init = 185
 box_y_init = 150
 box_y_offset = 45
 box_x_offset = 270
 
+
 # Declaration of labels for first column
-Label(App, justify=RIGHT, text="T:\n\n\na:\n\n\nK:\n\n\nL:\n\n\nJdimax:\n\n\nJdomax:\n\n\nJrmax:\n\n\nMs:"
-      ).grid(row=0, column=0, padx=label_x_init, pady=label_y_init)
+Label(App, justify=RIGHT, text="T:\n\n\na:\n\n\nK:\n\n\nL:\n\n\n" + Argumentos[0] + ":\n\n\n" +
+                               Argumentos[1] + ":\n\n\n" + Argumentos[2] + ":\n\n\nMs:").\
+    grid(row=0, column=0, padx=label_x_init, pady=label_y_init)
 
 # Declaration of labels for second column
 Label(App, justify=RIGHT, text="Kc:\n\n\nTi:\n\n\nTd:\n\n\nβ:\n\n\n\n\n\n\n\n\nα:\n\n\nγ:"
@@ -50,9 +67,29 @@ button_L = Entry(App)
 button_L.grid(row=3, column=1)
 button_L.place(x=box_x_init, y=box_y_init+3*box_y_offset)
 
-button_Jdimax = Entry(App)
-button_Jdimax.grid(row=4, column=1)
-button_Jdimax.place(x=box_x_init, y=box_y_init+4*box_y_offset)
+# Define Combobox for Ms values
+menu_Optimizar = ttk.Combobox(App, values=["Jdimax", "Jdomax", "Jrmax"])
+menu_Optimizar.grid(row=4, column=1)
+menu_Optimizar.place(x=box_x_init, y=box_y_init+4*box_y_offset)
+menu_Optimizar.set("Jdimax")
+menu_Optimizar.configure(width=17)
+
+if menu_Optimizar.current() == 0:
+    Argumentos[1] = 'Jdomax'
+    Argumentos[2] = 'Jrmax'
+elif menu_Optimizar.current() == 1:
+    Argumentos[1] = 'Jdimax'
+    Argumentos[2] = 'Jrmax'
+elif menu_Optimizar.current() == 2:
+    Argumentos[1] = 'Jdimax'
+    Argumentos[2] = 'Jdomax'
+# Para obtener el valor actual
+
+#print menu_Optimizar.current()
+
+# button_Jdimax = Entry(App)
+# button_Jdimax.grid(row=4, column=1)
+# button_Jdimax.place(x=box_x_init, y=box_y_init+4*box_y_offset)
 
 button_Jdomax = Entry(App)
 button_Jdomax.grid(row=5, column=1)
@@ -93,5 +130,8 @@ button_alpha.place(x=box_x_init+box_x_offset, y=box_y_init+6*box_y_offset)
 button_gamma = Entry(App)
 button_gamma.grid(row=7, column=2)
 button_gamma.place(x=box_x_init+box_x_offset, y=box_y_init+7*box_y_offset)
+
+activate_button = Button(App, text="Toca para actualizar", width=20, command=actualizar)
+activate_button.grid(row=0, column=5)
 
 App.mainloop()
--- getPIDdata.py
+++ getPIDdata.py
@@ -1,5 +1,5 @@
 import mysql.connector
-import os
+# import os
 
 connection = mysql.connector.connect(host='localhost',
                                      user='root',
@@ -7,13 +7,122 @@ connection = mysql.connector.connect(host='localhost',
                                      db='paretodatabase')
 
 cursor = connection.cursor()
-a = 1
-t0 = 0.3
+T = 0.7
+a = 0.5
+K = 1
+L = 0.21
+Jdimax = 0.6
+Jdomax = 0.5
+Jrmax = 0
+t0 = L/T
 Ms = 1.6
+# Los normalizados son las columnas 9,10,11
 
-cursor.execute("SELECT * from paretopruebatabla where a = %s and (Ms between %s-0.001 and %s+0.001) and t0 = %s;",
-               (a, Ms, Ms, t0))
+# obtencion de los parametros de la base de datos en funcion de Ms
+
+cursor.execute("SELECT * from paretopruebatabla where (Ms between %s-0.001 and %s+0.001);",
+               (Ms, Ms))
 resultados = cursor.fetchall()
-for resultado in resultados:
-        print resultado[0], resultado[5]
 
+# Averiguar cual de los cuatro casos tenemos
+for resultado in resultados:
+
+
+# Caso optimizando Jr
+if Jrmax == 0:
+    # Inicializacion de banderas
+    maximo_Jdi = 0
+    maximo_Jdo = 0
+    minimo_Jdi = 10
+    minimo_Jdo = 10
+
+    # Normalizar los J dados por el usuario
+    for resultado in resultados:
+        if maximo_Jdi < resultado[6]:
+            maximo_Jdi = float(resultado[6])
+        if maximo_Jdo < resultado[7]:
+            maximo_Jdo = float(resultado[7])
+        if minimo_Jdi > resultado[6]:
+            minimo_Jdi = float(resultado[6])
+        if minimo_Jdo > resultado[7]:
+            minimo_Jdo = float(resultado[7])
+
+    Jdinorm_usuario = (Jdimax - minimo_Jdi)/(maximo_Jdi - minimo_Jdi)
+    Jdonorm_usuario = (Jdomax - minimo_Jdo)/(maximo_Jdo - minimo_Jdo)
+
+# Caso optimizando Jdi
+elif Jdimax == 0:
+    # Inicializacion de banderas
+    maximo_Jr = 0
+    maximo_Jdo = 0
+    minimo_Jr = 10
+    minimo_Jdo = 10
+
+    # Normalizar los J dados por el usuario
+    for resultado in resultados:
+        if maximo_Jr < resultado[8]:
+            maximo_Jr = resultado[8]
+        if maximo_Jdo < resultado[7]:
+            maximo_Jdo = resultado[7]
+        if minimo_Jr > resultado[8]:
+            minimo_Jr = resultado[8]
+        if minimo_Jdo > resultado[7]:
+            minimo_Jdo = resultado[7]
+
+    Jrnorm_usuario = (Jrmax - minimo_Jr) / (maximo_Jr - minimo_Jr)
+    Jdonorm_usuario = (Jdomax - minimo_Jdo) / (maximo_Jdo - minimo_Jdo)
+
+# Caso optimizando Jdo
+elif Jdomax == 0:
+    # Inicializacion de banderas
+    maximo_Jr = 0
+    maximo_Jdi = 0
+    minimo_Jr = 10
+    minimo_Jdi = 10
+
+    # Normalizar los J dados por el usuario
+    for resultado in resultados:
+        if maximo_Jr < resultado[8]:
+            maximo_Jr = resultado[8]
+        if maximo_Jdi < resultado[6]:
+            maximo_Jdi = resultado[6]
+        if minimo_Jr > resultado[8]:
+            minimo_Jr = resultado[8]
+        if minimo_Jdi > resultado[6]:
+            minimo_Jdi = resultado[6]
+
+    Jrnorm_usuario = (Jrmax - minimo_Jr) / (maximo_Jr - minimo_Jr)
+    Jdinorm_usuario = (Jdimax - minimo_Jdi) / (maximo_Jdi - minimo_Jdi)
+
+# print maximo_Jdi, minimo_Jdi, Jdinorm_usuario, Jdonorm_usuario
+
+# Seleccionar los errores normalizados mas parecidos de la base de datos
+
+# Optimizar para Jr
+if Jrmax == 0:
+    Jrmax = 2 #Tengo que cambiar por un 1 cuando la normalizacion funcione jaja
+    for resultado in resultados:
+    #     print resultado[0], resultado[1], resultado[5], resultado[13], resultado[6], resultado[7], resultado[8]
+        if resultado[9] <= Jdimax and resultado[10] <= Jdomax and resultado[11] <=Jrmax:
+            Jrmax = resultado[11]
+            identificador = resultado[13]
+    # print identificador
+    # print maximo_Jdi, minimo_Jdi, maximo_Jdo, minimo_Jdo
+
+# Optimizar para Jdi
+if Jdimax == 0:
+    Jdimax = 2 #Tengo que cambiar por un 1 cuando la normalizacion funcione jaja
+    for resultado in resultados:
+        if resultado[9] <= Jdimax and resultado[10] <= Jdomax and resultado[11] <=Jrmax:
+            Jdimax = resultado[9]
+            identificador = resultado[13]
+    # print identificador
+
+# Optimizar para Jdo
+if Jdomax == 0:
+    Jdomax = 2 #Tengo que cambiar por un 1 cuando la normalizacion funcione jaja
+    for resultado in resultados:
+        if resultado[9] <= Jdimax and resultado[10] <= Jdomax and resultado[11] <=Jrmax:
+            Jdomax = resultado[10]
+            identificador = resultado[13]
+    # print identificador
